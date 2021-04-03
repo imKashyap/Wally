dc27f42acfc212a6ece98cc4a128ebcd07244b4c
Rahul Kashyap <rahulkashyap4041@gmail.com>
2021-04-04 01:35:52 +0530

    Added Page Controller

--- lib/screens/categories_page.dart
+++ lib/screens/categories_page.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
 
 class CategoriesPage extends StatelessWidget {
+  List<String> _categories = [
+    'Abstract',
+    'Animals',
+    'Beach',
+    'Bikes',
+    'Brands',
+    'Buildings',
+    'Cars',
+    'Celebrity',
+    'Games',
+    'Landscapes',
+    'Love',
+    'Miscellaneous',
+    'Movies',
+    'Music',
+    'Nature',
+    'Planes',
+    'Seasons',
+    'Sports',
+    'Texture',
+    'Water'
+  ];
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
-        backgroundColor: Color(0xFF111820),
-      body: Center(child: Text('Categories'),),
+    return Scaffold(
+      backgroundColor: Color(0xFF111820),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView.separated(
+          itemBuilder: (context, int index) =>
+              _buildCategoriesContainer(_categories[index], context),
+          itemCount: _categories.length,
+          separatorBuilder: (context, int index) => SizedBox(
+            height: 15.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoriesContainer(String title, BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(35.0),
+      decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      child: Text(
+        title,
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.headline5,
+      ),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:wally/screens/categories_page.dart';
 import 'package:wally/screens/latest_page.dart';
 import 'package:wally/screens/user_page.dart';
@@ -11,6 +11,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedPage = 0;
+  PageController _pageController;
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Map<String, dynamic>> _pages = [
@@ -20,53 +33,67 @@ class _HomePageState extends State<HomePage> {
     ];
 
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-          backgroundColor: Color(0xFF111820),
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(_pages[_selectedPage]['title'],
-                style: Theme.of(context).textTheme.headline4),
-          )),
-      body: _pages[_selectedPage]['page'],
-      // bottomNavigationBar: BottomNavigationBar(
-      //   onTap: _selectPage,
-      //   items: [
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.home_outlined),
-      //         activeIcon: Icon(Icons.home_filled),
-      //         label: 'Home'),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.category_outlined),
-      //         activeIcon: Icon(Icons.category),
-      //         label: 'Categories'),
-      //     BottomNavigationBarItem(
-      //         icon: Icon(Icons.person_outline_outlined),
-      //         activeIcon: Icon(Icons.person),
-      //         label: 'User'),
-      //   ],
-      //   backgroundColor: Colors.transparent,
-      //   selectedItemColor: Color(0xFFF3AA4E),
-      //   currentIndex: _selectedPage,
-      //   showSelectedLabels: false,
-      //   showUnselectedLabels: false,
-      //   type: BottomNavigationBarType.fixed,
-      // ),
-      bottomNavigationBar: CurvedNavigationBar(
-        onTap: _selectPage,
-        backgroundColor: Color(0xFF111820),
-        // color: Colors.red.shade900,
-        height: 60.0,
-        color: Color(0xFF316542),
-        items: [
-          Icon(Icons.home_outlined),
-          Icon(Icons.category_outlined),
-          Icon(Icons.person_outline_outlined),
-        ],
-        animationCurve: Curves.bounceInOut,
-        animationDuration: Duration(milliseconds: 300),
-      ),
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            backgroundColor: Color(0xFF111820),
+            title: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(_pages[_selectedPage]['title'],
+                  style: Theme.of(context).textTheme.headline6),
+            )),
+        //body: _pages[_selectedPage]['page'],
+        body: SizedBox.expand(
+          child: PageView(
+            controller: _pageController,
+            onPageChanged: _selectPage,
+            children: <Widget>[
+              _pages[0]['page'],
+              _pages[1]['page'],
+              _pages[2]['page'],
+            ],
+          ),
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(color: Colors.transparent, boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+          ]),
+          child: SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+              child: GNav(
+                  rippleColor: Colors.grey[500],
+                  hoverColor: Colors.grey[900],
+                  gap: 8,
+                  activeColor: Colors.white,
+                  iconSize: 24,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                  duration: Duration(milliseconds: 400),
+                  tabBackgroundColor: Color(0xFF111820),
+                  tabs: [
+                    GButton(
+                      icon: Icons.home_outlined,
+                      text: 'Home',
+                    ),
+                    GButton(
+                      icon: Icons.category_outlined,
+                      text: 'Categories',
+                    ),
+                    GButton(
+                      icon: Icons.person_outline_outlined,
+                      text: 'User',
+                    ),
+                  ],
+                  selectedIndex: _selectedPage,
+                  onTabChange: (index) {
+                    setState(() {
+                      _selectedPage = index;
+                      _pageController.jumpToPage(index);
+                    });
+                  }),
+            ),
+          ),
+        ));
   }
 
   void _selectPage(int index) {
